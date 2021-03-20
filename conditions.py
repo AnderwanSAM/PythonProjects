1aa64d7a60ddb08527d4d73244527c394c5ff9a6
AnderwanSAM <71939367+AnderwanSAM@users.noreply.github.com>
2021-03-20 18:42:20 -0400

    simple programme about conditions statements in Python

---
+++ conditions.py
@@ -0,0 +1,102 @@
+
+#conditions statements simples programms by Andie SAMADOULOUGOU 
+
+
+
+#leap year 
+year = int (input ("Give a year "))
+
+#results of division 
+
+by4 = float(year/4)
+by400 = float(year/400)
+by100 = float(year/100)
+
+#restes 
+r4 = by4 - int(by4)
+r400 = by400 - int(by400)
+r100 = by100 - int(by100)
+
+isLeap = False 
+
+print("by4 - int (by4)") 
+print(r4)
+print("by400 - int (by400)") 
+print(r400)
+print("by100 - int (by100)") 
+print(r100)
+
+
+# tester les resultats pour savoir su l'annee est bissectile 
+
+if r4 == 0 :
+	isLeap = True
+	if r100 == 0 :
+		isLeap = True
+	else:
+		isLeap = False
+		if r400 == 0 :
+		    isLeap = True
+		else:
+		    isLeap = False
+else : 
+	isLeap = False
+
+if(isLeap) : 
+	print("Leap Year")
+else : 
+	print("Not a leap Year")
+
+
+# Treasure island game 
+
+
+
+print("let's play a game of adventure")
+
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure. This is a lightly or a bit heavily modified version of the one you might know. :) ")
+choice1 = input('You\'re at a cross road. Where do you want to go? Type "left" or "right" \n').lower()
+if choice1 == "left":
+  choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower()
+  if choice2 == "wait":
+    choice3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower()
+    if choice3 == "red":
+      print("It's a room full of fire. Game Over.")
+    elif choice3 == "yellow":
+      print("You found the treasure room ! You are about to Win!Brace Yourself, answer this riddle and you will prove Yourself worthy of the treasure  ")
+      final_choice = input("Only one color, but not one size. Stuck at the bottom, yet I easily fly. Present in sun, but not in rain. Doing no harm, and feeling no pain. What Am I? :  \n").lower()
+      if final_choice == "shadow" :
+      	print("Congratulations! You win! The treasure is Yours!")
+      else : 
+      	print("the answer was shadow")
+    elif choice3 == "blue":
+      print("You enter a room of beasts. Game Over.")
+    else:
+      print("You chose a door that doesn't exist. Game Over.")
+  else:
+    print("You get attacked by an angry trout. Game Over.")
+else:
+  print("You fell into a hole. Game Over.")
