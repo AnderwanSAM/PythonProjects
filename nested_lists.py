c3d8e2b1564229cf3f094b15964885daec1672d5
AnderwanSAM <71939367+AnderwanSAM@users.noreply.github.com>
2021-03-20 20:20:00 -0400

    nested lists in python are a bit similar to 2D arrays in Python

---
+++ nested_lists.py
@@ -0,0 +1,89 @@
+
+
+#This is an exercice found on a bootcamp about nested lists 
+
+
+# Initialisation
+
+
+row1 = ["⬜️","⬜️","⬜️"]
+row2 = ["⬜️","⬜️","⬜️"]
+row3 = ["⬜️","⬜️","⬜️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? Enter the column first then the row ")
+
+#modifying the nested list 
+
+column = int(position[0]) - 1 
+row = int(position[1]) - 1
+
+map[row][column] = "X"
+
+
+
+
+#result 
+print(f"{row1}\n{row2}\n{row3}")
+
+
+
+# Treasure Map
+"""
+https://cdn.fs.teachablecdn.com/wiFJAkZZSG2RpGsxYgDO
+
+Instructions
+You are going to write a program which will mark a spot with an X.
+
+In the starting code, you will find a variable called map.
+
+This map contains a nested list.
+When map is printed this is what the nested list looks like:
+
+['⬜️', '⬜️', '⬜️'],['⬜️', '⬜️', '⬜️'],['⬜️', '⬜️', '⬜️']
+In the starting code, we have used new lines (\n) to format the three rows into a square, like this:
+
+['⬜️', '⬜️', '⬜️']
+['⬜️', '⬜️', '⬜️']
+['⬜️', '⬜️', '⬜️']
+This is to try and simulate the coordinates on a real map.
+
+
+
+Your job is to write a program that allows you to mark a square on the map using a two-digit system. The first digit is the vertical column number and the second digit is the horizontal row number. e.g.:
+
+https://cdn.fs.teachablecdn.com/2vnboIYTFFruvl9FJ2w5
+
+First your program must take the user input and convert it to a usable format.
+
+Next, you need to use it to update your nested list with an "x".
+
+Example Input 1
+column 2, row 3 would be entered as:
+
+23
+Example Output 1
+['⬜️', '⬜️', '⬜️']
+
+['⬜️', '⬜️', '⬜️']
+
+['⬜️', 'X', '⬜️']
+Example Input 2
+column 3, row 1 would be entered as:
+
+31
+Example Output 2
+['⬜️', '⬜️', 'X']
+
+['⬜️', '⬜️', '⬜️']
+
+['⬜️', '⬜️', '⬜️']
+e.g. When you hit run, this is what should happen:
+
+https://cdn.fs.teachablecdn.com/5hliFjyIR96LdestyfPd
+
+Hint
+Remember that Lists start at index 0!
+map is just a variable that contains a nested list. It's not related to the map function in Python
+"""
+
